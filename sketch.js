57eca3587396f990ab58b6ea8b1653629c3cd6bb
ShambhaviSingh21 <75373885+ShambhaviSingh21@users.noreply.github.com>
2021-02-09 21:27:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject, roof;
 var rope1,rope2,rope3, rope4,rope5;
 var world;
 
@@ -18,7 +17,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(width/2,height/4,width/7,20);
+	roofObject= new roof(width/2,height/4,width/7,20);
 
 	bobDiameter=40;
 
@@ -52,51 +51,7 @@ function setup() {
 	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
 	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
 
-	/*constraint1={
-		bodyA:bobObject1.body,
-		bodyB:roofObject.body,
-		pointB: {x:-bobDiameter*2, y:0}
-	}
-
-	constraint2={
-		bodyA:bobObject2.body,
-		bodyB:roofObject.body,		
-		pointB: {x:-bobDiameter, y:0}
-	}
-
-
-	constraint3={
-		bodyA:bobObject3.body,
-		bodyB:roofObject.body,		
-		pointB: {x:0, y:0}
-
-	}
-
-	constraint4={
-		bodyA:bobObject4.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter, y:0}	
-
-	}
-
-	constraint5={
-		bodyA:bobObject5.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter*2, y:0}
-	}
-
-	var pendulum1=Constraint.create(constraint1)
-	var pendulum2=Constraint.create(constraint2)
-	var pendulum3=Constraint.create(constraint3)
-	var pendulum4=Constraint.create(constraint4)
-	var pendulum5=Constraint.create(constraint5)
-
-	World.add(world, pendulum1);
-	World.add(world, pendulum2);
-	World.add(world, pendulum3);
-	World.add(world, pendulum4);
-	World.add(world, pendulum5);
-	*/
+	
 	Engine.run(engine);
 	//Render.run(render);
   
@@ -118,15 +73,7 @@ function draw() {
   bobObject3.display();
   bobObject4.display();
   bobObject5.display();
- 
-  
-  
-	
-  
- 
-  
-  
- 
+
 }
 
 function keyPressed() {
@@ -149,9 +96,3 @@ function drawLine(constraint)
 	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
 	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
 }
-
-
-
-
-
-
